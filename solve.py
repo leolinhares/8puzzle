cb84bf224d9c5be7db8c7e609747b49151d78587
Leo Linhares <slinharesleo@gmail.com>
2015-10-25 12:35:38 -0300

    creating the main function

--- solve.py
+++ solve.py
@@ -1,22 +1,27 @@
 from puzzle_solver import Solver
 import time
-start_time = time.time()
 
-Solver.draw_path(Solver.breadth_first_search())
-# node_state, all_paths = Solver.a_star()
 
-# lista = []
-#
-# def find_parent(son):
-#     for parent, child in all_paths.iteritems():
-#         if child == son:
-#             lista.append(parent)
-#             find_parent(parent)
+def main():
+    start_time = time.time()
+    Solver.draw_path(Solver.breadth_first_search())
+    # node_state, all_paths = Solver.a_star()
 
-# print(node_state)
-# print(len(all_paths))
-# find_parent(node_state)
-# print(lista)
+    # lista = []
+    #
+    # def find_parent(son):
+    #     for parent, child in all_paths.iteritems():
+    #         if child == son:
+    #             lista.append(parent)
+    #             find_parent(parent)
 
-# Solver.draw_path(all_paths[-1])
-print("--- %s seconds ---" % (time.time() - start_time))
+    # print(node_state)
+    # print(len(all_paths))
+    # find_parent(node_state)
+    # print(lista)
+
+    # Solver.draw_path(all_paths[-1])
+    print("--- %s seconds ---" % (time.time() - start_time))
+
+if __name__ == '__main__':
+    main()
