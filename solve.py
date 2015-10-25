3e5ae206b299f1f39af9f234c260a8669b11ce74
Leo Linhares <slinharesleo@gmail.com>
2015-10-25 13:00:40 -0300

    basic menu. needs polishing

--- solve.py
+++ solve.py
@@ -3,8 +3,35 @@ import time
 
 
 def main():
-    start_time = time.time()
-    Solver.draw_path(Solver.breadth_first_search())
+    option = True
+    while option:
+        print("""
+            1. Solve a randomly generated puzzle using BFS
+            2. Solve a randomly generated puzzle using A* (hamming heuristic)
+            3. Solve a randomly generated puzzle using A* (manhattan heuristic)
+
+            Press enter to quit
+            """)
+        option = input("\nWhat do you like to do?")
+        if option == "1":
+            start_time = time.time()
+            Solver.draw_path(Solver.breadth_first_search())
+            print("--- %s seconds ---" % (time.time() - start_time))
+
+        elif option == "2":
+            start_time = time.time()
+            node_state, all_paths = Solver.a_star()
+            print(node_state)
+            print(len(all_paths))
+            print("--- %s seconds ---" % (time.time() - start_time))
+        elif option == "3":
+            pass
+        elif option != "":
+            print("\nInvalid choice")
+
+
+    # start_time = time.time()
+    # Solver.draw_path(Solver.breadth_first_search())
     # node_state, all_paths = Solver.a_star()
 
     # lista = []
@@ -21,7 +48,8 @@ def main():
     # print(lista)
 
     # Solver.draw_path(all_paths[-1])
-    print("--- %s seconds ---" % (time.time() - start_time))
+    # print("--- %s seconds ---" % (time.time() - start_time))
+
 
 if __name__ == '__main__':
     main()
